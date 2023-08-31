f97f833ef20da3b37ffccf64cb9863101ccf63cf
Alexey Sokolov <alexvyber@gmail.com>
2023-08-31 13:39:52 +0300

    update ladle

--- packages/ladle/src/index.ts
+++ packages/ladle/src/index.ts
@@ -1,7 +1,7 @@
-export function getOptions<T extends Record<string, string>>(arg: T): Array<keyof T> {
-  return (Object.keys(arg) as Array<keyof typeof arg>).map((key) => key)
+export function getOptions<T extends object>(arg: T): Array<keyof T> {
+  return Object.keys(arg) as Array<keyof T>
 }
 
 export function getFirstValue<T extends object>(variants: T): keyof T {
-  return (Object.keys(variants) as Array<keyof T>)[0]
+  return getOptions(variants)[0]
 }
